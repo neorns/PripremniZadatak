23f9e73c429c43033f442a840ff247f5864681c0
Nenad <norlovic@gmail.com>
2017-03-10 18:37:35 +0100

    aktivnosti

---
+++ app/src/main/java/com/example/androiddevelopment/pripremnizadatak/activities/DetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.androiddevelopment.pripremnizadatak.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.androiddevelopment.pripremnizadatak.R;
+
+/**
+ * Created by androiddevelopment on 10.3.17..
+ */
+
+public class DetailActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
